b3e67eae92b30b91c8dadd381c9bdc9af70fd2a6
fernandadlfuente <157684670+fernandadlfuente@users.noreply.github.com>
2025-12-03 01:39:27 -0500

    Add console logs and functions in main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
+console.log("Hello World!");
+console.log("I am " + 21 + " years old.");
+console.log(3/2);
+console.log(0.1 + 0.2);
+if(true){
+    let x = 10;
+    const y = 20;
+    console.log(x);
+    console.log(y);
+}
+
+function parentFunc() {
+const a = 1;
+
+function nestedFunc(){
+    const b = 4;
+    return a + b;
+}
+return nestedFunc();
+
+}
+ console.log(parentFunc());
+function Fullname(firstname, lastname) {
+    return firstname + " "  + lastname;
+}
+
+console.log(Fullname("Fernanda", "De La Fuente"));
+
+
+const array = [1, 2, 3, 4, 5];
+
+console.log(array);
+console.log(array[2]);
 
